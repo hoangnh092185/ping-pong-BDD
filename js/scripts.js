27559ebc351b4644b175a37376555c3ef848e1db
Nhat Hoang <hoangnh092185@gmail.com>
2016-08-19 06:41:24 -0700

    transfer the input from the local logic to global logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 // ****Business logic****
+function numberString(input){
+  for(var i= 0; i<=input;i++)
+    if (input === "3"){
+      alert("true");
+    }alert(false);
 
-
+};
+debugger;
 
 
 
@@ -10,6 +16,7 @@ $(document).ready(function(){
   $(".form-group").submit(function(event){
     event.preventDefault();
     var numberInput = $("input").val();
+    numberString(numberInput);
 
     $("#results").append(numberArray);
   })
